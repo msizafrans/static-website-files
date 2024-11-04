c04589d7e86344240a1b205f32c05fe71f802835
Frans <135562158+msizafrans@users.noreply.github.com>
2024-11-04 10:03:53 +0200

    Update jquery.js

--- vendor/jquery/jquery.js
+++ vendor/jquery/jquery.js
@@ -11,7 +11,18 @@
  *
  * Date: 2018-01-20T17:24Z
  */
-(ire("jquery")(window);
+( function( global, factory ) {
+
+	"use strict";
+
+	if ( typeof module === "object" && typeof module.exports === "object" ) {
+
+		// For CommonJS and CommonJS-like environments where a proper `window`
+		// is present, execute the factory and get jQuery.
+		// For environments that do not have a `window` with a `document`
+		// (such as Node.js), expose a factory as module.exports.
+		// This accentuates the need for the creation of a real `window`.
+		// e.g. var jQuery = require("jquery")(window);
 		// See ticket #14549 for more info.
 		module.exports = global.document ?
 			factory( global, true ) :
@@ -10351,5 +10362,3 @@ if ( !noGlobal ) {
 
 return jQuery;
 } );
-
-dsdskhe
